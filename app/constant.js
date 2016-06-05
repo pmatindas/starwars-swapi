d16a4d2f3659369be254aef89a75368819a44576
Pingkan <p.matindas@gmail.com>
2016-06-05 22:26:43 +0700

    add trailling slash into constant

--- app/constant.js
+++ app/constant.js
@@ -3,10 +3,10 @@
 
     angular.module('starwarsApp')    	
         .constant('starwarsConfig',{        	
-        	swapiPerson:'https://swapi.co/api/people',
-        	swapiStarship: 'https://swapi.co/api/starships',
-        	swapiFilm: 'https://swapi.co/api/films',
-        	swapiSpecies: 'https://swapi.co/api/species'
+        	swapiPerson:'https://swapi.co/api/people/',
+        	swapiStarship: 'https://swapi.co/api/starships/',
+        	swapiFilm: 'https://swapi.co/api/films/',
+        	swapiSpecies: 'https://swapi.co/api/species/'
         });
 
 })();
